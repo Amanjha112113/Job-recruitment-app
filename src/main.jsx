3c1d2fda3170fe3b980e16a1b009ffaefa120977
Aman Jha <amanjhanothing112113@gmail.com>
2026-02-19 00:24:21 +0530

    Hardcode Google Client ID fallback to debug env var issue

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,13 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "get-your-client-id-from-google-cloud-console"}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "245133775908-f57l6o1vp0kcna6lr0lhkmfs711pathb.apps.googleusercontent.com"}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
+
+
+
